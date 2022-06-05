bcd7d402dbb4aaa0476a8593f8ebb3fc7345e0af
Ryu Hanaoka <doradora56@gmail.com>
2022-06-05 23:08:34 +0900

    プロジェクト ファイルを追加します。

---
+++ src/FizzBuzz/FizzBuzz.Test/FizzBuzzTest.cs
@@ -0,0 +1,14 @@
+using Xunit;
+using FluentAssertions;
+
+namespace FizzBuzz.Test
+{
+    public class FizzBuzzTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
