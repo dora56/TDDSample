a24c1b5e18e2cdbd42ddbc847217ad409ae07141
Ryu Hanaoka <doradora56@gmail.com>
2022-06-12 23:59:11 +0900

    gteen:

--- src/FizzBuzz/FizzBuzz.Test/FizzBuzzTest.cs
+++ src/FizzBuzz/FizzBuzz.Test/FizzBuzzTest.cs
@@ -1,14 +1,21 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 using FluentAssertions;
 
 namespace FizzBuzz.Test
 {
     public class FizzBuzzTest
     {
-        [Fact]
-        public void Test1()
+        [Fact(DisplayName = "1を渡すと文字列1を返す")]
+        public void NumberToString()
         {
-            Assert.True(true);
+            // 準備
+            var fizzBuzz = new FizzBuzz();
+            // 実効
+
+            var actual = fizzBuzz.Convert(1);
+            // 検証
+            Assert.Equal("1", actual);
         }
     }
 }
---
+++ src/FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public string Convert(int i)
+        {
+            return "1";
+        }
+    }
+}
